fdf92624122c3a9694e84079cafe480fc8e68a43
Maksim Kruglov <maximchik666@gmail.com>
2023-02-06 16:41:14 +0400

    Initial Commit

---
+++ SwiftUITeachingProject/SwiftUITeachingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITeachingProjectApp.swift
+//  SwiftUITeachingProject
+//
+//  Created by Maksim Kruglov on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITeachingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
